bb4b63d8ebd984076998c1e75803fbd87609f800
Finley Stewart <finley.stewart99@gmail.com>
2019-11-01 16:58:43 -0400

    Scaffolding: layout files + icons

--- app/src/main/java/com/finleystewart/eventfinleyyasseen/MainActivity.kt
+++ app/src/main/java/com/finleystewart/eventfinleyyasseen/MainActivity.kt
@@ -2,6 +2,9 @@ package com.finleystewart.eventfinleyyasseen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.content.Intent
+import android.net.Uri
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +12,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu, menu)
+
+        val intent: Intent = Intent(Intent.ACTION_VIEW)
+        intent.setData(Uri.parse("https://www.dawsoncollege.qc.ca/"))
+
+        // Sets link to WWW option
+        menu.findItem(R.id.item4).setIntent(
+            intent
+        )
+
+        return true;
+    }
+
 }
