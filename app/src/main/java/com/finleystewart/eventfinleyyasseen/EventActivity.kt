2839e5e460b520624b721d9f400685ec930c7fc4
Finley Stewart <finley.stewart99@gmail.com>
2019-11-11 21:42:22 -0500

    changes to eventActivity

--- app/src/main/java/com/finleystewart/eventfinleyyasseen/EventActivity.kt
+++ app/src/main/java/com/finleystewart/eventfinleyyasseen/EventActivity.kt
@@ -5,12 +5,29 @@ import android.os.Bundle
 import android.view.Menu
 import android.content.Intent
 import android.net.Uri
+import android.widget.TextView
+import com.finleystewart.eventfinleyyasseen.business.Event
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class EventActivity : Activity() {
+
+    val lateinit event : Event
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var intent = intent
+
+        event = intent.extras!!.get("event")
+
+        val shortDesc = findViewById<TextView>(R.id.shortDesc)
+        val longDesc = findViewById<TextView>(R.id.longDesc)
+        val date = findViewById<TextView>(R.id.date)
+
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener( event ->  )
+
     }
 
 }
